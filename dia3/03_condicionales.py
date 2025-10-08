8b0ec4f661dd3fed106738166427ee13129f5ccd
Christian Rua <cborisa1425@gmail.com>
2025-10-08 13:18:48 -0500

    Agregar carpetas dia1 y dia3 al mismo nivel en la rama main

---
+++ dia3/03_condicionales.py
@@ -0,0 +1,20 @@
+#PROGRAMA QUE SUME 2 NUMEROS ENTEROS
+
+# DATOS DE ENTRADA(INPUT)
+
+numero1 = input("ingrese el primer numero entero: ")
+numero2 = input("ingrese el Segundo numero entero: ")
+operacion = input("ingrese la operacion(+, -, *, /)")
+# PROCESO
+if(operacion == "+"):
+    resultado = numero1 + numero2
+elif(operacion == "-"):
+    resultado = numero1 + numero2   
+elif(operacion == "*"):
+    resultado = numero1 + numero2   
+elif(operacion == "/"):
+    resultado = numero1 + numero2   
+    
+# DARTOS DE SALIDA(OUTPUT)
+
+print(f"el resultado de {numero1} {operacion} {numero2} es: " {resultado})
---
+++ dia3/04_buclefor.py
@@ -0,0 +1,6 @@
+# BUCLE FOR
+
+for contador in range(1, 10):
+    print(contador)
+
+
