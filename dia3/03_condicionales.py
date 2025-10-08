9a3c86f4636778ba0c3b2474431ac1a02df30233
Christian Rua <cborisa1425@gmail.com>
2025-10-08 13:38:43 -0500

    Actualizar script condicionales en dia3

--- dia3/03_condicionales.py
+++ dia3/03_condicionales.py
@@ -1,20 +1,21 @@
-#PROGRAMA QUE SUME 2 NUMEROS ENTEROS
+# PROGRAMA QUE SUME 2 NÚMEROS ENTEROS
 
-# DATOS DE ENTRADA(INPUT)
+# DATOS DE ENTRADA (INPUT)
+numero1 = int(input("Ingrese el primer número entero: "))
+numero2 = int(input("Ingrese el segundo número entero: "))
+operacion = input("Ingrese la operación (+, -, *, /): ")
 
-numero1 = input("ingrese el primer numero entero: ")
-numero2 = input("ingrese el Segundo numero entero: ")
-operacion = input("ingrese la operacion(+, -, *, /)")
 # PROCESO
-if(operacion == "+"):
+if operacion == "+":
     resultado = numero1 + numero2
-elif(operacion == "-"):
-    resultado = numero1 + numero2   
-elif(operacion == "*"):
-    resultado = numero1 + numero2   
-elif(operacion == "/"):
-    resultado = numero1 + numero2   
-    
-# DARTOS DE SALIDA(OUTPUT)
+elif operacion == "-":
+    resultado = numero1 - numero2
+elif operacion == "*":
+    resultado = numero1 * numero2
+elif operacion == "/":
+    resultado = numero1 / numero2
+else:
+    resultado = "Operación no válida"
 
-print(f"el resultado de {numero1} {operacion} {numero2} es: " {resultado})
+# DATOS DE SALIDA (OUTPUT)
+print(f"El resultado de {numero1} {operacion} {numero2} es: {resultado}")
