0db3139c8252f8d8dc5afb8d60cd23cbe70bac6d
Christian Rua <cborisa1425@gmail.com>
2025-10-13 19:55:34 -0500

    nuevo archivo dia5

---
+++ dia5/01_funciones.py
@@ -0,0 +1,20 @@
+# FUNCIONES EN PYTHON
+
+
+
+def saludar(nombre):
+    """Funciones que saluda a una persona por su nombre."""
+    return f"Hola, {nombre}"
+    
+nombre_usurario = input("Hola, ¿como te llamas?")
+print(saludar(nombre_usurario))
+
+def sumar(a,b):
+    """Funcion que suma dos numeros"""
+    resultado = a + b
+    return resultado
+
+num1 = float(input("Ingrese el primer numero1: "))
+num2 = float(input("Ingrese el primer numero2: "))
+resultado_suma = sumar(num1,num2)
+print(f"La suma de {num1} + {num2} es:")
