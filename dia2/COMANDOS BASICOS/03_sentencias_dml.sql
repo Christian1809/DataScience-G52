e7e56d28a8a7e95db200b79a1c54c7fbf55b43db
Christian Rua <cborisa1425@gmail.com>
2025-10-17 22:25:27 -0500

    data2

---
+++ dia2/COMANDOS BASICOS/03_sentencias_dml.sql
@@ -0,0 +1,19 @@
+--SENTENCIAS DML
+
+
+
+--INSERT
+INSERT INTO alumno(nro_documneto,nombre) VALUES('100','cesar')
+
+--INSERTAR VARIOS REGISTROS
+INSERT INTO alumno(nro_documneto,nombre,nota)
+VALUES
+('200','ana',15),
+('300','luis',20),
+('400','jose',11),
+('500','raul',10),
+('600','carmen',13),
+('700','jorge',16),
+('800','daniel',20),
+('900','luisa',17),
+('1000','pedro',5);
